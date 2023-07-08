547b261a6c21af8ff799462302dd797a83791e49
“Zamin” <mzamin26@gmail.com>
2023-07-08 15:52:11 +0530

    backup for other storage

--- cron.sh
+++ cron.sh
@@ -2,7 +2,9 @@
 
 # Prepare cronjob
 
-CRON_TIME="$1"
+# https://crontab.guru/#0_*/12_*_*_*, Use this website to modify the time, right now it's config to run every 12 hours.
+
+CRON_TIME="0 */12 * * *"
 CRON_CMD="bash /var/wp-backup/backup.sh >> /var/wp-backup/backup.log 2>&1"
 CRON_JOB="$CRON_TIME $CRON_CMD"
 
---
+++ wb-backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Assign values to parameters that will be used in Script
+DATE="$(date +%Y-%m-%d)"
+BACKUP_DIR=/var/backup
+SERVER_HOSTNAME=""
+NODE=
+REMOTE=""
+
+# Create fresh backup directory
+mkdir -p "$BACKUP_DIR/$DATE"
+
+echo "Starting BACKUP Creation and Upload to Selected Storage"
+echo $DATE
+start=$SECONDS
+# ls -1 /var/www/ -Ihtml -I22222 | while read user; do
+    webinoly -backup=local -export -destination=$BACKUP_DIR/$DATE
+    wait
+    echo $BACKUP_DIR/$DATE
+    echo $REMOTE/$SERVER_HOSTNAME/$DATE
+    rclone copy $BACKUP_DIR/$DATE $REMOTE/$SERVER_HOSTNAME/$DATE
+    wait
+    rm -f $BACKUP_DIR/*
+    wait
+    rclone delete $REMOTE/$SERVER_HOSTNAME --min-age 30d
+    wait
+# done
+echo "Backup Creation and Upload to Selected Storage Finished"
+duration=$((SECONDS - start))
+echo "Total Time Taken $duration Seconds"
+
+# Remove backup directory
+rm -rf $BACKUP_DIR/$DATE
+
+exit
